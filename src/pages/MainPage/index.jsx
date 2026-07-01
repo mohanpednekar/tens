227fecaa945a8ba8f589826636e0ecef385b16d9
Mohan Namdeo Pednekar <mohanpednekar@users.noreply.github.com>
2026-07-01 22:55:52 +0530

    Update index.jsx
    Signed-off-by: Mohan Namdeo Pednekar <mohanpednekar@users.noreply.github.com>

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,7 +2,7 @@ import Button from 'components/Button'
 import Money from 'components/Money'
 import StatCard from 'components/StatCard'
 import { formatAmount, getAutobuyerCost, getTierCost, getTierPurchasedCount, getTierSpendableAmount, isTierUnlocked, productionMultiplier } from 'game/engine'
-import { MONEY_ID, PRESTIGE_PP_COST, RESOURCE_NAMES, RESOURCE_SYMBOL, TIER_DEFINITIONS } from 'game/layers'
+import { MONEY_ID, PRESTIGE_PP_COST, RESOURCE_SYMBOL, TIER_DEFINITIONS } from 'game/layers'
 import { useIncrementalGame } from 'game/useIncrementalGame'
 import styled from 'styled-components'
 
