e227e94ed5fc984d81c4c9684fe8658edcedf8ed
Mohan Namdeo Pednekar <mohanpednekar@users.noreply.github.com>
2026-07-05 00:03:13 +0530

    Update engine.js

--- src/game/engine.js
+++ src/game/engine.js
@@ -7,7 +7,6 @@ const allResourceIds = () => {
   const ids = new Set([MONEY_ID])
   TIER_DEFINITIONS.forEach(t => {
     ids.add(t.costResourceId)
-    ids.add(t.producesResourceId)
   })
   return [...ids]
 }
