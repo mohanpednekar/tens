108b0291c148c646eb2deca468a995c53eaa37f0
Mohan Namdeo Pednekar <mohanpednekar@users.noreply.github.com>
2026-07-04 23:57:14 +0530

    Update layers.js

--- src/game/layers.js
+++ src/game/layers.js
@@ -1,24 +1,25 @@
 // Each tier above Ones spends the immediately lower resource it produces.
 export const TIER_DEFINITIONS = [
   // 'tens' intentionally has costResourceId === producesResourceId: it is the
-  // entry-level money generator, bought with ones to produce more ones.
-  { id: 'Tens',            symbol:'Tens', baseCost: 10,  costResourceId: 'ones', producesResourceId: 'ones' },
-  { id: 'Thousands',       symbol:'Ks', baseCost: 1E3, costResourceId: 'ones', producesResourceId: 'Tens' },
-  { id: 'Millions',        symbol:'Ms', baseCost: 1E6, costResourceId: 'ones', producesResourceId: 'Thousands' },
-  { id: 'Billions',         symbol:'Bs', baseCost: 1E9,  costResourceId: 'ones',  producesResourceId: 'Millions' },
-  { id: 'Trillions',       symbol:'Ts', baseCost: 1E12,costResourceId: 'ones',  producesResourceId: 'Billions' },
-  { id: 'Quadrillions',     symbol:'Qs', baseCost: 1E15,  costResourceId: 'ones',  producesResourceId: 'Trillions' },
-  { id: 'Pentillions',      symbol:'Ps', baseCost: 1E18,  costResourceId: 'ones',  producesResourceId: 'Quadrillions' },
-  { id: 'Hexillions',       symbol:'Hs', baseCost: 1E21,  costResourceId: 'ones',  producesResourceId: 'Pentillions' },
-  { id: 'Septillions',       symbol:'Ss', baseCost: 1E24,  costResourceId: 'ones',  producesResourceId: 'Hexillions' },
-  { id: 'Octillions',       symbol:'Os', baseCost: 1E27,  costResourceId: 'ones',  producesResourceId: 'Septillions' },
-  { id: 'Nonillions',       symbol:'Ns', baseCost: 1E30,  costResourceId: 'ones',  producesResourceId: 'Octillions' },
-  { id: 'Decillions',       symbol:'Ds', baseCost: 1E33,  costResourceId: 'ones',  producesResourceId: 'Nonillions' },
+  // entry-level money generator, bought with Ones to produce more Ones.
+  { id: 'Ones',            symbol:'$', baseCost: 1,  costResourceId: 'Ones', producesResourceId: 'Cents' },
+  { id: 'Tens',            symbol:'Tens', baseCost: 10,  costResourceId: 'Ones', producesResourceId: 'Ones' },
+  { id: 'Thousands',       symbol:'Ks', baseCost: 1E3, costResourceId: 'Ones', producesResourceId: 'Tens' },
+  { id: 'Millions',        symbol:'Ms', baseCost: 1E6, costResourceId: 'Ones', producesResourceId: 'Thousands' },
+  { id: 'Billions',         symbol:'Bs', baseCost: 1E9,  costResourceId: 'Ones',  producesResourceId: 'Millions' },
+  { id: 'Trillions',       symbol:'Ts', baseCost: 1E12,costResourceId: 'Ones',  producesResourceId: 'Billions' },
+  { id: 'Quadrillions',     symbol:'Qs', baseCost: 1E15,  costResourceId: 'Ones',  producesResourceId: 'Trillions' },
+  { id: 'Pentillions',      symbol:'Ps', baseCost: 1E18,  costResourceId: 'Ones',  producesResourceId: 'Quadrillions' },
+  { id: 'Hexillions',       symbol:'Hs', baseCost: 1E21,  costResourceId: 'Ones',  producesResourceId: 'Pentillions' },
+  { id: 'Septillions',       symbol:'Ss', baseCost: 1E24,  costResourceId: 'Ones',  producesResourceId: 'Hexillions' },
+  { id: 'Octillions',       symbol:'Os', baseCost: 1E27,  costResourceId: 'Ones',  producesResourceId: 'Septillions' },
+  { id: 'Nonillions',       symbol:'Ns', baseCost: 1E30,  costResourceId: 'Ones',  producesResourceId: 'Octillions' },
+  { id: 'Decillions',       symbol:'Ds', baseCost: 1E33,  costResourceId: 'Ones',  producesResourceId: 'Nonillions' },
 ]
 
 
 export const RESOURCE_SYMBOL = tierId => TIER_DEFINITIONS.find(t => t.id = tierId).symbol || '$'
-export const MONEY_ID = 'ones'
+export const MONEY_ID = 'Ones'
 export const MONEY_STARTING_AMOUNT = 10
 export const PRESTIGE_PP_COST = 10
 export const TICK_RATE_MS = 1000
